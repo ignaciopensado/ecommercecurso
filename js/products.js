61d9d0e8d93c3cc27f749a708279956802f8e638
ignaciopensado <nachopensado96@gmail.com>
2020-08-30 06:37:55 -0300

    entrega 2

--- js/products.js
+++ js/products.js
@@ -1,32 +1,105 @@
-fetch(PRODUCTS_URL)
-        .then(respuesta=>respuesta.json())
-        .then(datos=>{
-            document.getElementById('interno').innerHTML = construirProductos(datos);
-        })
-        .catch(error=>alert("Ocurrió un error: " + error));
+const ORDEN_NORMAL = "Normal";
+const ORDEN_POR_SOLD_COUNT = "Cant.";
+const ORDEN_DESC_POR_COST = "+ a -";
+const ORDEN_ASC_POR_COST = "- a +";
 
-function construirProductos(info){
-    var eldiv = document.getElementById('contenedor');
-    for(var i=0; i<info.length; i++){
-        var linea = `    <br>
-                        <div class="borde">
-                            <h1>${info[i].name} - <i>${info[i].currency} ${info[i].cost}</i></h1>                                             
-                            <p style="font-size:22px;"><img class="achicar" src="${info[i].imgSrc}" onclick="irAlCarrito();" title="CLICK para agregar al Carrito"</img>${info[i].description}</p>                      
-                            <p class="vendidos">${info[i].soldCount} vendidos.</p>
-                        </div>       
-                        <br>
-                   `
-        eldiv.innerHTML += linea;
-    }
-}
+var currentProductsArray = [];
+var currentSortCriterio = undefined;
+
+var cotaMin = undefined;
+var cotaMax = undefined;
+
 
 function irAlCarrito(){
     window.location = 'cart.html';
 }
 
-function busqueda() {
-   var b = document.getElementById("buscado");
-   document.getElementById("barra").innerHTML = b.value;
+
+//FUNCION PARA ORDENAR ARREGLO
+function ordenarCat(criterio, arreglo) {
+    let result = [];
+    //si el criterio es el normal, muestro como viene
+    if (criterio === ORDEN_NORMAL) {
+        result = arreglo;
+    } 
+    //si el criterio son los vendidos...
+    else if (criterio === ORDEN_POR_SOLD_COUNT) {
+        result = arreglo.sort(function (a, b) {
+            let aCount = parseInt(a.soldCount);
+            let bCount = parseInt(b.soldCount);
+
+            if (aCount > bCount) {
+                return -1;
+            }
+            if (aCount < bCount) {
+                return 1;
+            }
+            return 0;
+        });
+    }
+    //si el criterio es descendente por precio...
+    else if (criterio === ORDEN_DESC_POR_COST) {
+        result = arreglo.sort(function (a, b) {
+            if (a.cost > b.cost) {
+                return -1;
+            }
+            if (a.cost < b.cost) {
+                return 1;
+            }
+            return 0;
+        });
+    }
+    //si el criterio es ascendente por precio...
+    else if (criterio === ORDEN_ASC_POR_COST) {
+        result = arreglo.sort(function (a, b) {
+            if (a.cost < b.cost) {
+                return -1;
+            }
+            if (a.cost > b.cost) {
+                return 1;
+            }
+            return 0;
+        });
+    }
+    return result;
+}
+
+
+//FUNCION PARA MOSTRAR LOS PRODUCTOS
+function showCategoriesList() {
+    let htmlParaAnexar = "";
+    for (let i = 0; i < currentCategoriesArray.length; i++) {
+        let producto = currentCategoriesArray[i];
+        //chequeo si quiero mostrar acotado
+        if (((cotaMin == undefined) || (cotaMin != undefined && parseInt(producto.cost) >= cotaMin)) &&
+            ((cotaMax == undefined) || (cotaMax != undefined && parseInt(producto.cost) <= cotaMax))) {
+                 htmlParaAnexar += `
+                        <br>
+                        <div class="borde">
+                            <h1>${producto.name} - <i>${producto.currency} ${producto.cost}</i></h1>                                             
+                            <p style="font-size:22px;"><img class="achicar" src="${producto.imgSrc}" onclick="irAlCarrito();" title="CLICK para agregar al Carrito"</img>${producto.description}</p>                      
+                            <p class="vendidos">${producto.soldCount} vendidos.</p>
+                        </div>       
+                        <br>
+                   `
+        }
+        document.getElementById("interno").innerHTML = htmlParaAnexar;
+    }
+}
+
+
+//FUNCION QUE COMBINA AMBAS
+function sortAndShowCategories(sortCriterio, categoriesArray) {
+    currentSortCriterio = sortCriterio;
+
+    if (categoriesArray != undefined) {
+        currentCategoriesArray = categoriesArray;
+    }
+
+    currentCategoriesArray = ordenarCat(currentSortCriterio, currentCategoriesArray);
+
+    //Muestro las categorías ordenadas
+    showCategoriesList();
 }
 
 
@@ -34,5 +107,52 @@ function busqueda() {
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+   getJSONData(PRODUCTS_URL).then(function (resultObj) {
+       if (resultObj.status === "ok") {
+           sortAndShowCategories(ORDEN_NORMAL, resultObj.data);
+       }
+   });
+    
+   document.getElementById("ordenarPorVendidos").addEventListener("click", function () {
+            sortAndShowCategories(ORDEN_POR_SOLD_COUNT);
+    });
+    
+    document.getElementById("bOpcion").addEventListener("click", function () {
+        if(document.getElementById("opciones").value == "desc"){
+            sortAndShowCategories(ORDEN_DESC_POR_COST);
+        }
+        else{
+            sortAndShowCategories(ORDEN_ASC_POR_COST);
+        }
+    });
+    
+    document.getElementById("rangeFilterCount").addEventListener("click", function () {
+        //Obtengo el mínimo y máximo de los intervalos para filtrar por cantidad
+        //de productos por categoría.
+        cotaMin = document.getElementById("rangeFilterCountMin").value;
+        cotaMax = document.getElementById("rangeFilterCountMax").value;
+
+        if ((cotaMin != undefined) && (cotaMin != "") && (parseInt(cotaMin)) >= 0) {
+            cotaMin = parseInt(cotaMin);
+        } else {
+            cotaMin = undefined;
+        }
+
+        if ((cotaMax != undefined) && (cotaMax != "") && (parseInt(cotaMax)) >= 0) {
+            cotaMax = parseInt(cotaMax);
+        } else {
+            cotaMax = undefined;
+        }
+
+        showCategoriesList();
+    });
+    
+    document.getElementById("buscar").addEventListener("click", function () {
+                var texto = document.getElementById("barra").value;
+                window.find(texto);
+     });
 
 });
+
+
+
